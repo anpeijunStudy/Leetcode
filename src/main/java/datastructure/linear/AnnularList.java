a0068a1e67f45a4be2921cf77bc30345bf336d7b
anpeijun <1740606965@qq.com>
2022-08-10 21:07:06 +0800

    刷题第八天-第二次提交

---
+++ src/main/java/datastructure/linear/AnnularList.java
@@ -0,0 +1,13 @@
+package datastructure.linear;
+
+/**
+ * @author anpeijun
+ * @version 1.0
+ * @description 环形链表
+ * @date 2022/08/10/21:05
+ */
+public class AnnularList {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/datastructure/linear/DoubleLinkedListDemo.java
+++ src/main/java/datastructure/linear/DoubleLinkedListDemo.java
@@ -1,4 +1,27 @@
 package datastructure.linear;
 
 public class DoubleLinkedListDemo {
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer> linkedList = new DoubleLinkedList<>();
+
+        linkedList.insert(1);
+        linkedList.insert(3);
+        linkedList.insert(1, 2);
+        linkedList.insert(4);
+        linkedList.insert(5);
+        linkedList.insert(7);
+        linkedList.insert(5, 6);
+
+
+        System.out.println("迭代器遍历链表：");
+        for (Integer integer : linkedList) {
+            System.out.println(integer);
+        }
+
+//        System.out.println("删除6元素：" + linkedList.remove(5));
+        System.out.println("元素5的索引：" + linkedList.indexOf(5));
+        System.out.println("首节点：" + linkedList.firstNode());
+        System.out.println("首节点：" + linkedList.lastNode());
+        System.out.println("中间值："+linkedList.getMid());
+    }
 }
